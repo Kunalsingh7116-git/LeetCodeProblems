4cd4de3118e31a88084abed28fb2d1f3ca3273d1
Kunal Singh <singhkunal7116@gmail.com>
2026-07-26 19:59:12 +0530

    Time: 24 ms (5.59%) | Memory: 17.9 MB (6.45%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<basic_string<char>, int> mpp{{"M",1000},{"CM",900},{"D",500},{"CD",400},
-                                      {"C",100},{"XC",90},{"L",50},{"XL",40} ,
+        unordered_map<string,int> mpp{{"M",1000},{"CM",900},{"D",500},{"CD",400},
+                                      {"C",100},{"XC",90},{"L",50},{"XL",40},
                                       {"X",10},{"IX",9},{"V",5},{"IV",4},{"I",1}};
         int result = 0;
         int i = 0;
