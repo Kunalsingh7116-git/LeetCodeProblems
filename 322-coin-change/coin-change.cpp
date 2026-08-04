7f99a8bdb0c74c2cf84e38aa5db1ff5135ecc7a3
Kunal Singh <singhkunal7116@gmail.com>
2026-08-05 02:35:43 +0530

    Time: 11 ms (98.36%) | Memory: 17.8 MB (94.44%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -13,20 +13,29 @@ public:
         // vector <vector<int>>dp(n+1,vector<int>(amount+1,0));
         //intialize row by 1e9
         //intialize col by 0
-        vector <int> prev(amount+1,0);
-        for(int j = 0 ; j <= amount ;j++) prev[j] = 1e9;
-        // for(int i = 0 ; i <= n ;i++) dp[i][0] = 0;
+        // vector <int> prev(amount+1,0);
+        // for(int j = 0 ; j <= amount ;j++) prev[j] = 1e9;
+        // // for(int i = 0 ; i <= n ;i++) dp[i][0] = 0;
         
 
-        for(int i = 1;i <= n;i++){
-            vector<int> curr(amount+1,0);
-            for(int j = 1; j <= amount;j++){
-                if(j-coins[i-1] >= 0) curr[j] = min(prev[j], 1 + curr[j-coins[i-1]]);
-                else curr[j] = prev[j];   
-            }
-            prev = curr;
-        }
-        return prev[amount] >= 1e9 ? -1 : prev[amount];
+        // for(int i = 1;i <= n;i++){
+        //     vector<int> curr(amount+1,0);
+        //     for(int j = 1; j <= amount;j++){
+        //         if(j-coins[i-1] >= 0) curr[j] = min(prev[j], 1 + curr[j-coins[i-1]]);
+        //         else curr[j] = prev[j];   
+        //     }
+        //     prev = curr;
+        // }
+        // return prev[amount] >= 1e9 ? -1 : prev[amount];
         // return findMinCoins(coins,amount,n,dp) >= 1e9 ? -1 : findMinCoins(coins,amount,n,dp);
+        vector <int> dp(amount+1,1e9);
+        dp[0] = 0;
+        for(int i = 0; i < n ;i++){
+            for(int j = coins[i]; j<= amount; j++){
+                dp[j] = min(dp[j], 1+dp[j-coins[i]]);
+            }
+        }
+        return dp[amount] >= 1e9 ? -1 : dp[amount];
+
     }
 };
