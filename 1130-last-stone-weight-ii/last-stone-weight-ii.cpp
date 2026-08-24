ed641be371c9a512350f0c40f60a8f9bacd8543b
Kunal Singh <singhkunal7116@gmail.com>
2026-08-24 22:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (56.77%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    int minDiff(int sum1,vector<int>& stones,int totalSum,int n,vector<vector<int>>&dp){
-        if(n == 0){
-            int sum2 = totalSum - sum1;
-            return abs(sum1-sum2);
-        }
+    // int minDiff(int sum1,vector<int>& stones,int totalSum,int n,vector<vector<int>>&dp){
+    //     if(n == 0){
+    //         int sum2 = totalSum - sum1;
+    //         return abs(sum1-sum2);
+    //     }
 
-        if(dp[n][sum1] != -1) return dp[n][sum1];
+    //     if(dp[n][sum1] != -1) return dp[n][sum1];
 
-        return dp[n][sum1] = min(minDiff(sum1+stones[n-1],stones,totalSum,n-1,dp), minDiff(sum1,stones,totalSum,n-1,dp));
-    }
+    //     return dp[n][sum1] = min(minDiff(sum1+stones[n-1],stones,totalSum,n-1,dp), minDiff(sum1,stones,totalSum,n-1,dp));
+    // }
     int lastStoneWeightII(vector<int>& stones) {
         int n = stones.size();
-
-        int sum1 = 0;
 
         int totalSum = 0;
 
         for(int i = 0; i < n;i++) totalSum += stones[i];
 
-        vector<vector <int>> dp(n+1,vector<int>(totalSum+1,-1));
+        int target = totalSum/2;
 
-        return minDiff(sum1,stones,totalSum,n,dp);
+        vector <int> dp(target+1,0);
+       
+        dp[0] = 1;
+
+        for(int i : stones){
+            for(int j = target; j >= i ;j--){
+                dp[j] = dp[j] || dp[j-i];
+            }
+        }
+
+        int best = 0;
+
+        for(int j = target; j >= 0;j--){
+            if(dp[j]) {
+                best = j ;
+                break;
+            }
+        }
+
+        return totalSum - 2 * best;
     }
 
 };
